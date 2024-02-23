d87db2dd89f750c8d33d9428fee0495d24474832
beltran.061035 <beltran.061035@LAB-A-PC26>
2024-02-23 09:25:45 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace StudentGradeApplication
+{
+    partial class frmStudentGradeProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameText = new System.Windows.Forms.Label();
+            this.GradText = new System.Windows.Forms.Label();
+            this.EngText = new System.Windows.Forms.Label();
+            this.MatText = new System.Windows.Forms.Label();
+            this.SciText = new System.Windows.Forms.Label();
+            this.FilText = new System.Windows.Forms.Label();
+            this.HisText = new System.Windows.Forms.Label();
+            this.NameCon = new System.Windows.Forms.TextBox();
+            this.EngCon = new System.Windows.Forms.TextBox();
+            this.MatCon = new System.Windows.Forms.TextBox();
+            this.SciCon = new System.Windows.Forms.TextBox();
+            this.FilCon = new System.Windows.Forms.TextBox();
+            this.HisCon = new System.Windows.Forms.TextBox();
+            this.GenAvBut = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameText
+            // 
+            this.NameText.AutoSize = true;
+            this.NameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameText.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.NameText.Location = new System.Drawing.Point(18, 14);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(43, 13);
+            this.NameText.TabIndex = 0;
+            this.NameText.Text = "Name:";
+            // 
+            // GradText
+            // 
+            this.GradText.AutoSize = true;
+            this.GradText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GradText.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.GradText.Location = new System.Drawing.Point(160, 60);
+            this.GradText.Name = "GradText";
+            this.GradText.Size = new System.Drawing.Size(77, 24);
+            this.GradText.TabIndex = 1;
+            this.GradText.Text = "Grades";
+            this.GradText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EngText
+            // 
+            this.EngText.AutoSize = true;
+            this.EngText.Location = new System.Drawing.Point(18, 104);
+            this.EngText.Name = "EngText";
+            this.EngText.Size = new System.Drawing.Size(41, 13);
+            this.EngText.TabIndex = 2;
+            this.EngText.Text = "English";
+            // 
+            // MatText
+            // 
+            this.MatText.AutoSize = true;
+            this.MatText.Location = new System.Drawing.Point(145, 104);
+            this.MatText.Name = "MatText";
+            this.MatText.Size = new System.Drawing.Size(31, 13);
+            this.MatText.TabIndex = 3;
+            this.MatText.Text = "Math";
+            // 
+            // SciText
+            // 
+            this.SciText.AutoSize = true;
+            this.SciText.Location = new System.Drawing.Point(274, 104);
+            this.SciText.Name = "SciText";
+            this.SciText.Size = new System.Drawing.Size(46, 13);
+            this.SciText.TabIndex = 4;
+            this.SciText.Text = "Science";
+            // 
+            // FilText
+            // 
+            this.FilText.AutoSize = true;
+            this.FilText.Location = new System.Drawing.Point(18, 155);
+            this.FilText.Name = "FilText";
+            this.FilText.Size = new System.Drawing.Size(39, 13);
+            this.FilText.TabIndex = 5;
+            this.FilText.Text = "Filipino";
+            // 
+            // HisText
+            // 
+            this.HisText.AutoSize = true;
+            this.HisText.Location = new System.Drawing.Point(145, 155);
+            this.HisText.Name = "HisText";
+            this.HisText.Size = new System.Drawing.Size(39, 13);
+            this.HisText.TabIndex = 6;
+            this.HisText.Text = "History";
+            // 
+            // NameCon
+            // 
+            this.NameCon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NameCon.Location = new System.Drawing.Point(65, 12);
+            this.NameCon.Name = "NameCon";
+            this.NameCon.Size = new System.Drawing.Size(320, 20);
+            this.NameCon.TabIndex = 7;
+            // 
+            // EngCon
+            // 
+            this.EngCon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.EngCon.Location = new System.Drawing.Point(65, 102);
+            this.EngCon.Name = "EngCon";
+            this.EngCon.Size = new System.Drawing.Size(64, 20);
+            this.EngCon.TabIndex = 8;
+            // 
+            // MatCon
+            // 
+            this.MatCon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MatCon.Location = new System.Drawing.Point(192, 102);
+            this.MatCon.Name = "MatCon";
+            this.MatCon.Size = new System.Drawing.Size(64, 20);
+            this.MatCon.TabIndex = 9;
+            // 
+            // SciCon
+            // 
+            this.SciCon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SciCon.Location = new System.Drawing.Point(321, 102);
+            this.SciCon.Name = "SciCon";
+            this.SciCon.Size = new System.Drawing.Size(64, 20);
+            this.SciCon.TabIndex = 10;
+            // 
+            // FilCon
+            // 
+            this.FilCon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FilCon.Location = new System.Drawing.Point(65, 153);
+            this.FilCon.Name = "FilCon";
+            this.FilCon.Size = new System.Drawing.Size(64, 20);
+            this.FilCon.TabIndex = 11;
+            // 
+            // HisCon
+            // 
+            this.HisCon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HisCon.Location = new System.Drawing.Point(192, 153);
+            this.HisCon.Name = "HisCon";
+            this.HisCon.Size = new System.Drawing.Size(64, 20);
+            this.HisCon.TabIndex = 12;
+            // 
+            // GenAvBut
+            // 
+            this.GenAvBut.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenAvBut.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.GenAvBut.Location = new System.Drawing.Point(277, 138);
+            this.GenAvBut.Name = "GenAvBut";
+            this.GenAvBut.Size = new System.Drawing.Size(108, 47);
+            this.GenAvBut.TabIndex = 13;
+            this.GenAvBut.Text = "Generate\r\nAverage";
+            this.GenAvBut.UseVisualStyleBackColor = true;
+            this.GenAvBut.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(21, 204);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(364, 73);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "             ";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmStudentGradeProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(405, 298);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.GenAvBut);
+            this.Controls.Add(this.HisCon);
+            this.Controls.Add(this.FilCon);
+            this.Controls.Add(this.SciCon);
+            this.Controls.Add(this.MatCon);
+            this.Controls.Add(this.EngCon);
+            this.Controls.Add(this.NameCon);
+            this.Controls.Add(this.HisText);
+            this.Controls.Add(this.FilText);
+            this.Controls.Add(this.SciText);
+            this.Controls.Add(this.MatText);
+            this.Controls.Add(this.EngText);
+            this.Controls.Add(this.GradText);
+            this.Controls.Add(this.NameText);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "frmStudentGradeProgram";
+            this.Text = "Student Grade Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameText;
+        private System.Windows.Forms.Label GradText;
+        private System.Windows.Forms.Label EngText;
+        private System.Windows.Forms.Label MatText;
+        private System.Windows.Forms.Label SciText;
+        private System.Windows.Forms.Label FilText;
+        private System.Windows.Forms.Label HisText;
+        private System.Windows.Forms.TextBox NameCon;
+        private System.Windows.Forms.TextBox EngCon;
+        private System.Windows.Forms.TextBox MatCon;
+        private System.Windows.Forms.TextBox SciCon;
+        private System.Windows.Forms.TextBox FilCon;
+        private System.Windows.Forms.TextBox HisCon;
+        private System.Windows.Forms.Button GenAvBut;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGradeApplication
+{
+    public partial class frmStudentGradeProgram : Form
+    {
+        public frmStudentGradeProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double average = 0;
+            average += Convert.ToDouble(EngCon.Text);
+            average += Convert.ToDouble(MatCon.Text);
+            average += Convert.ToDouble(SciCon.Text);
+            average += Convert.ToDouble(FilCon.Text);
+            average += Convert.ToDouble(HisCon.Text);
+            average /= 5;
+
+            label8.Text = (average > 75 ? "The student passed." : "The student failed.") + "\nThe general average of " + NameCon.Text + " is " + average + "." ;
+            if (average > 75)
+            {
+                label8.Text = "The student passed.\nThe general average of " + NameCon.Text + " is " + average + ".";
+            }
+
+            else 
+            {
+                label8.Text = "The student failed.\nThe general average of " + NameCon.Text + " is " + average + ".";
+            }
+        }
+    }
+}
